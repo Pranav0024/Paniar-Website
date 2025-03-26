c058dea64957710012cb3a67cf0e7b6d0890ed57
Pranav0024 <39133940+Pranav0024@users.noreply.github.com>
2025-03-26 21:12:39 +0530

    auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
+// app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
 import externalAuthOptions from "@/lib/configs/auth/authOptions";
 
-export const handler = NextAuth(externalAuthOptions);
+const handler = NextAuth(externalAuthOptions);
+
 export { handler as GET, handler as POST };
